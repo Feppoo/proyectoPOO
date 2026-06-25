e537925d932d5aeda95b4e5eee2956ecbc1014c4
Feppoo <167021596+Feppoo@users.noreply.github.com>
2026-06-25 17:25:56 -0600

    Update Main Frame
    Convertida a solo una ventana por comodidad

---
+++ src/interfaz/VentanaMain.java
@@ -0,0 +1,59 @@
+package interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+
+public class VentanaMain {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaMain window = new VentanaMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel panelPrestamos = new JPanel();
+		tabbedPane.addTab("Préstamos", null, panelPrestamos, null);
+		
+		JPanel panelReportes = new JPanel();
+		tabbedPane.addTab("Reportes", null, panelReportes, null);
+		
+		JPanel panelAdmin = new JPanel();
+		tabbedPane.addTab("Administrador", null, panelAdmin, null);
+	}
+
+}
